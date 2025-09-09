9173c1ed1996fbda6bb22a27a46595a1d7cade80
pablogallardodev <pablito.260197@gmail.com>
2025-09-08 22:06:05 -0600

    (feature) Migración de proyecto a react 19 con Vite

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Router from './routes/Router';
+import './css/index.css';
+
+function App() {  
+  return <Router />
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import firebase from "firebase";
+import App from './App.jsx'
+
+const { VITE_FIREBASE_CONFIG } = import.meta.env
+
+// Iniciamos firebaseApp
+firebase.initializeApp(JSON.parse(VITE_FIREBASE_CONFIG));
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
